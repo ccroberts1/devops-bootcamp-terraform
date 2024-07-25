ae2ac8a0fb7f5afc79c9df4d774bee1ec7fe837b
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-07-25 12:05:44 -0600

    Add mysql and helm setup

---
+++ mysql.tf
@@ -0,0 +1,43 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+provider "helm" {
+  kubernetes {
+    host = data.aws_eks_cluster.cluster.endpoint
+    token = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
+
+resource "helm_release" "mysql" {
+  name       = "my-release"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "mysql"
+  version    = "9.14.0"
+  timeout    = "1000"
+
+  values = [
+    file("values.yaml")
+  ]
+
+  set {
+    name  = "volumePermissions.enabled"
+    value = true
+  }
+}
+
+
+
+
+
