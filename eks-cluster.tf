857928420eb29257e13b638c4d0d75beb62e12b2
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-07-25 12:34:31 -0600

    Update mysql and eks configs to fix dependency error for terraform apply

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,22 +1,20 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
-  version = "19.17.2"
+  version = "19.20.0"
 
   cluster_name = var.cluster_name
   cluster_version = var.k8s_version
-
-  cluster_addons = {
-    aws-ebs-csi-driver = {}
-  }
-
   cluster_endpoint_public_access = true
 
   subnet_ids = module.myapp-vpc.private_subnets
   vpc_id = module.myapp-vpc.vpc_id
-
   tags = {
     environment = var.env_prefix
     application = "myapp"
+  }
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {}
   }
 
   eks_managed_node_groups = {
@@ -31,11 +29,10 @@ module "eks" {
     }
 
     tags = {
-      Name = "${var.env_prefix}"
-    }
-
-    iam_role_additional_policies = {
-      AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      Name = var.env_prefix
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
     }
   }
 
